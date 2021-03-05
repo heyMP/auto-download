541df5cd0665c2bfc7606d23b5afe71ff1ac529f
heyMP <mgp140@gmail.com>
2021-03-05 17:20:27 -0500

    added github-corner

---
+++ github-corner/index.js
@@ -0,0 +1,3 @@
+import { GithubCorner } from "./github-corner.js";
+
+window.customElements.define(GithubCorner.tag, GithubCorner);
--- index.js
+++ index.js
@@ -3,3 +3,4 @@ import "https://unpkg.com/@patternfly/pfe-card/dist/pfe-card.min.js?module";
 import "https://unpkg.com/@patternfly/pfe-codeblock/dist/pfe-codeblock.min.js?module";
 import "https://unpkg.com/@patternfly/pfe-band/dist/pfe-band.min.js?module";
 import "./auto-download/index.js";
+import "./github-corner/index.js";
